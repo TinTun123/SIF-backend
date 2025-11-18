bc95ada9054f18c705595ef6cf8428481a11208b
TinTun123 <58549019+TinTun123@users.noreply.github.com>
2025-11-18 19:13:40 +0700

    FB Sharing media interviews

--- resources/views/share/media.blade.php
+++ resources/views/share/media.blade.php
@@ -10,9 +10,9 @@
     <!-- <meta property="og:image" content="{{ $interview->thumbnail }}"> -->
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:type" content="video">
-    <meta property="og:video" content="https://api.sif-mm.org/storage/thumbnails/db012cc1-6d56-47f7-810d-42a62b09bd64.jpg">
-    <meta property="og:video:url" content="https://api.sif-mm.org/storage/thumbnails/db012cc1-6d56-47f7-810d-42a62b09bd64.jpg">
-    <meta property="og:video:secure_url" content="https://api.sif-mm.org/storage/thumbnails/db012cc1-6d56-47f7-810d-42a62b09bd64.jpg">
+    <meta property="og:video" content="https://youtu.be/T-61Tuv9znU?si=lQr0J7vW2Nim3DNh">
+    <meta property="og:video:url" content="https://youtu.be/T-61Tuv9znU?si=lQr0J7vW2Nim3DNh">
+    <meta property="og:video:secure_url" content="https://youtu.be/T-61Tuv9znU?si=lQr0J7vW2Nim3DNh">
     <meta property="og:video:type" content="video/mp4">
     <meta property="og:video:width" content="1280">
     <meta property="og:video:height" content="720">
